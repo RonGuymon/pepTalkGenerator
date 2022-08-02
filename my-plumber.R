ced1c709c89a127770f65bb5b8767e4814e2dfde
Ron Guymon <rnguymon@gmail.com>
2022-08-01 23:59:02 -0700

    Added two files for deployment on remote AWS server.

---
+++ my-plumber.R
@@ -0,0 +1,20 @@
+# plumber.R
+#* @apiTitle Pep Talk
+#* @apiDescription Returns a random pep talk
+#* @get /pepTalk
+function(){
+  myList <- list(p1 = c("Champ,", "Fact:", "Everybody says", "Dang...", "Check it:", "Just saying...", "Superstar,", "Tiger,", "Self,", "Know this:", "News alert:", "Girl,", "Bro,", "Ace,", "Excuse me but", "Experts agree:", "In my opinion,", "Hear ye, hear ye:", "Okay, listen up:")
+                 , p2 = c("the mere idea of you", "your soul", "your hair today", "everything you do", "your personal style", "every thought you have", "that sparkle in your eye", "your presence here", "what you got going on", "the essential you", "your life's journey", "that saucy personality", "your DNA", "that brain of yours", "your choice of attire", "the way you roll", "whatever your secret is", "all of y'all")
+                 , p3 = c("has serious game", "rains magic,", "deserves the Nobel Prize,", "raises the roof,", "breeds miracles,", "is paying off big time,", "shows mad skills,", "just shimmers,", "is a national treasure,", "gets the party hopping,", "is the next big thing,", "roars like a lion,", "is a rainbow factory,", "is made of diamonds,", "makes birds sing,", "should be taught in school,", "makes my world go 'round,", "is 100% legit,")
+                 , p4 = c("24/7.", "can I get an amen?", "and that's a fact.", "so treat yourself.", "you feel me?", "that's just science.", "would I lie?", "for reals.", "mic drop.", "you hidden gem.", "snuggle bear.", "period.", "can you dig it?", "now let's dance.", "high five.", "say it again!", "according to CNN.", "so get used to it.")
+  )
+  # Randomly put them together----
+  saying <- c()
+  for(i in myList){
+    ts <- sample(i, 1)
+    saying <- paste(saying, ts, sep = ' ')
+  }
+  saying <- trimws(saying, which = 'both')
+  saying
+  return(saying)
+}
